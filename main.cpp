71ab71c505406b3772782dbf00e7dabb19e6f74d
Hamid Nazari <hn@linux.com>
2020-09-19 23:17:14 +1000

    List platforms and devices

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <OpenCL/cl.hpp>
 
 int main() {
-    cl::Platform platform;
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    assert(platforms.size() > 0);
+
+    auto platform = platforms.front();
+
+    std::vector<cl::Device> devices;
+    platforms.front().getDevices(CL_DEVICE_TYPE_GPU, &devices);
+
+    assert(devices.size() > 0);
+
+    auto device = devices.front();
+    auto vendor = device.getInfo<CL_DEVICE_VENDOR>();
+    auto version = device.getInfo<CL_DEVICE_VERSION>();
 
     return 0;
 }
